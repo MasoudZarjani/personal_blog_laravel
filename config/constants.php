7a8b82cc36640664d7437a0e9d0c14a52c5f65a8
masoud zarjani <masoud.mail.info@gmail.com>
2019-01-30 11:30:06 +0330

    add constants

---
+++ config/constants.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'relation' => [
+        'type' => [
+            'post' => 0,
+            'comment' => 1,
+        ],
+    ],
+    'post' => [
+        'status' => [
+            'passive' => 0,
+            'active' => 1,
+        ],
+        'type' => [
+            'blog' => 0
+        ]
+    ]
+];
